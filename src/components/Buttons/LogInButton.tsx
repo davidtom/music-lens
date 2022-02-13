c8de78a5885992145854152de187f69ae1eedb34
davidtom <davidjtomczyk@gmail.com>
2022-02-13 11:37:24 -0600

    Fix log in button - just a regular anchor tag since its doesnt need in-app routing

--- src/components/Buttons/LogInButton.tsx
+++ src/components/Buttons/LogInButton.tsx
@@ -1,13 +1,7 @@
-import Link from "next/link";
-
 // TODO: all button styles should be such that they look like buttons (and therefore dont
 // act/change after clicking like an anchor tag does)
 const LogInButton: React.FC = () => {
-  return (
-    <Link href={"/api/login"} passHref>
-      <a>Log In</a>
-    </Link>
-  );
+  return <a href={"/api/login"}>Log In</a>;
 };
 
 export default LogInButton;
