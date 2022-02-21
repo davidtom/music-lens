7dce90775547ced48f092208f2ab93e723c20c2f
davidtom <davidjtomczyk@gmail.com>
2022-02-20 21:39:23 -0600

    useRecentlyPlayed available for non-users

--- src/lib/useRecentlyPlayed.ts
+++ src/lib/useRecentlyPlayed.ts
@@ -1,27 +1,10 @@
 import useSWR from "swr";
 
-import type { SessionUser } from "lib/session";
+import { UserRecentlyPlayed } from "pages/api/users/[spotifyId]/recently-played";
 
-export type UserRecentlyPlayed = {
-  playedAt: string;
-  track: {
-    name: string;
-    durationMs: number;
-    album: {
-      name: string;
-    };
-    artists: {
-      artist: {
-        name: string;
-      };
-    }[];
-  };
-}[];
+export default function useRecentlyPlayed(userId: string) {
+  const { data: recentlyPlayed, mutate: mutateRecentlyPlayed } =
+    useSWR<UserRecentlyPlayed>(`/api/users/${userId}/recently-played`);
 
-export default function useRecentlyPlayed(user: SessionUser | undefined) {
-  const { data: recentlyPlayed } = useSWR<UserRecentlyPlayed>(
-    user?.isLoggedIn ? `/api/users/${user.id}/recently-played` : null
-  );
-
-  return { recentlyPlayed };
+  return { recentlyPlayed, mutateRecentlyPlayed };
 }
