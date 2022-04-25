ba84d596a1629aecc9e06cb9136047e47d5bed0a
davidtom <davidjtomczyk@gmail.com>
2022-04-24 23:02:03 -0500

    fix: change hostname to origin for sync script

--- src/jobs/sync-spotify-plays.sh
+++ src/jobs/sync-spotify-plays.sh
@@ -5,17 +5,17 @@ set -e
 # Log timestamp
 date -u
 
-HOSTNAME=$1
+ORIGIN=$1
 API_SECRET_KEY=$2
 
 # Get ids for all users in db
-json=`curl -s -H "Authorization: Basic $API_SECRET_KEY" -X GET  "http://$HOSTNAME/api/users"`
+json=`curl -s -H "Authorization: Basic $API_SECRET_KEY" -X GET  "$ORIGIN/api/users"`
 
 # Trigger a sync for each user
 # TODO: confirm this fails correctly
 for id in `echo "${json}" | jq -r '.[].id'`; do
   echo "Requesting for userId: ${id}"
-  results=`curl --silent --show-error -f -H "Authorization: Basic $API_SECRET_KEY" -X GET  "http://$HOSTNAME/api/sync/recently-played/${id}"; echo`
+  results=`curl --silent --show-error -f -H "Authorization: Basic $API_SECRET_KEY" -X GET  "$ORIGIN/api/sync/recently-played/${id}"; echo`
   echo "New plays: `echo ${results} | jq '.count'`"
 done
 echo
