0782a1adab71704548c4f71d9925beb385f6f8f9
davidtom <davidjtomczyk@gmail.com>
2022-04-24 22:57:26 -0500

    feat: update sync script to run via args and setup git cron workflow for prod

--- src/jobs/sync-spotify-plays.sh
+++ src/jobs/sync-spotify-plays.sh
@@ -2,19 +2,20 @@
 
 set -e
 
-# TODO: should this be a node script? or a "fun" chance to learn bash?
-
 # Log timestamp
 date -u
 
+HOSTNAME=$1
+API_SECRET_KEY=$2
+
 # Get ids for all users in db
-json=`curl -s -H "Authorization: Basic c3lzdGVtOm1hbmFnZQ==" -X GET  "http://localhost:8080/api/users"`
+json=`curl -s -H "Authorization: Basic $API_SECRET_KEY" -X GET  "http://$HOSTNAME/api/users"`
 
 # Trigger a sync for each user
 # TODO: confirm this fails correctly
 for id in `echo "${json}" | jq -r '.[].id'`; do
   echo "Requesting for userId: ${id}"
-  results=`curl --silent --show-error -f -H "Authorization: Basic c3lzdGVtOm1hbmFnZQ==" -X GET  "http://localhost:8080/api/sync/recently-played/${id}"; echo`
+  results=`curl --silent --show-error -f -H "Authorization: Basic $API_SECRET_KEY" -X GET  "http://$HOSTNAME/api/sync/recently-played/${id}"; echo`
   echo "New plays: `echo ${results} | jq '.count'`"
 done
 echo
